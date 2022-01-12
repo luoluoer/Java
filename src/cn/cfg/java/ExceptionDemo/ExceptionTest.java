3acf5e9ab55f88b21a35fe5dc0640571a7ea82fe
chaifanggui <1844203813@qq.com>
2022-01-12 22:42:28 +0800

    EXception

--- src/cn/cfg/java/ExceptionDemo/ExceptionTest.java
+++ src/cn/cfg/java/ExceptionDemo/ExceptionTest.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 * 异常处理机制的作用是,程序不崩溃,继续向下运行
 * 异常分为错误和异常两种,错误就是不能继续运行的叫做错误
 * 异常又分为编译时异常和执行时异常两种
+* 常见执行时异常:nullpointerexception arithmeticexception arrayindexoutofboundsexception
+* classcastexception stackoverflowexception
 * @author 柴方贵
 * @version 创建时间：2022年1月11日 下午10:19:19
 */
