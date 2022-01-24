53257e50241d0dc2dcfe05519865c5bfc8f588ab
chaifanggui <1844203813@qq.com>
2022-01-24 23:00:56 +0800

    StringBuffer

---
+++ src/cn/cfg/java/StringBuffer/StringAndStringBuffer.java
@@ -0,0 +1,45 @@
+
+package cn.cfg.java.StringBuffer;
+
+/**
+* 这是文档注释
+* @author 柴方贵
+* @version 创建时间：2022年1月24日 下午10:23:12
+*/
+public class StringAndStringBuffer {
+	public static void main(String[] args) {
+		//String转StringBuffer
+		String str = "Hellow World";
+		//1.方式1
+		StringBuffer sb1 = new StringBuffer(str);
+		//2.方式2
+		StringBuffer sb2 = new StringBuffer();
+		sb2.append(str);
+		
+		//StringBuffer转String
+		//1.方式1 toString
+		StringBuffer sb = new StringBuffer("柴方贵学习");
+		String strnew = sb.toString();
+		//2.使用String构造器
+		String strnew2 = new String(sb);
+		//StringBuffer常用方法
+		//1.append()
+		sb.append(10);
+		System.out.println(sb);
+		//2.delete()
+		sb.delete(1,2);//前闭后开区间
+		System.out.println(sb);
+		//3.replace() 
+		sb.replace(1,2,"hahahaha");
+		System.out.println(sb);
+		//4.indexOf(),指定的字符串第一次出现的索引,如果没有,那么返回-1
+		System.out.println(sb);
+		System.out.println(sb.indexOf("学习"));
+		System.out.println(sb.indexOf("笨蛋"));
+		//5.insert() ,在指定的位置插入
+		sb.insert(9,"二麻子");
+		System.out.println(sb);
+		//6.length(),返回长度
+		System.out.println(sb.length());
+	}
+}
--- src/cn/cfg/java/StringBuffer/StringBufferDemo.java
+++ src/cn/cfg/java/StringBuffer/StringBufferDemo.java
@@ -7,9 +7,24 @@ package cn.cfg.java.StringBuffer;
 * 在父类中, AbstractStringBuffer有属性char[]  value  不是final
 * 该value数组存放字符串内容,所以存放在堆中
 * StringBuffer是一个final类,不能被继承
+* 
+* StringBuffer的构造器主要有四个:
+* 1.StringBuffer(), 构造一个不带字符的字符串缓冲区,初始容量16个字符
+* 2.StringBuffer(Charsequence),构造一个字符串缓冲区,它包含与charsequence相同的字符
+* 基本没用
+* 3.StringBuffer(int capacity),构造一个字符串缓冲区,指定大小为capacity
+* 4.通过给String创建一个StringBuffer
 * @author 柴方贵
 * @version 创建时间：2022年1月23日 下午10:58:22
 */
 public class StringBufferDemo {
-
+	public static void main(String[] args) {
+		//1. 
+		StringBuffer sb1 = new StringBuffer();
+		//2.不介绍
+		//3.
+		StringBuffer sb3 = new StringBuffer(20);
+		
+		StringBuffer sb4 =  new StringBuffer("abcd");
+	}
 }
